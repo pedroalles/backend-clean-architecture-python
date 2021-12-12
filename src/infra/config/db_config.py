3dc4d002b384c1da43c7f4ac019109cb2c9d16de
pedroalles <pedroalles@gmail.com>
2021-12-12 20:31:40 -0300

    feat: add __start__ and __exit__ methods to DBConnectionHandler

--- src/infra/config/db_config.py
+++ src/infra/config/db_config.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 class DBConnectionHandler:
     """Sqlalchemy database connection"""
 
     def __init__(self) -> None:
-        self.__conection_string = "sqlite://storage.db"
+        self.__conection_string = "sqlite:///storage.db"
         self.session = None
 
     def get_engine(self):
@@ -15,3 +16,11 @@ class DBConnectionHandler:
         """
         engine = create_engine(self.__conection_string)
         return engine
+
+    def __start__(self):
+        session_maker = sessionmaker()
+        self.session = session_maker(self.get_engine())
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
